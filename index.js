a5d0694c043b308ce5158309564a9f9e45a8964b
Leko <leko@leko.moe>
2021-10-14 17:51:25 +0800

    feat: new link type

--- index.js
+++ index.js
@@ -3,19 +3,30 @@ const { Bot } = require('grammy')
 const bot = new Bot(process.env.token);
 
 function shortenUrl(url) {
+    const rules = [
+        [url => url.pathname === "/permalink.php", url => url.searchParams.get("story_fbid")],
+        [url => /^\/groups\/\d+\/?$/.test(url.pathname), url => url.searchParams.get("multi_permalinks")]
+    ];
+    const regexs = [
+        /^\/(?:.+)\/posts\/(\d+)\/?$/,
+        /^\/groups\/(?:.+)\/user\/(\d+)\/?$/,
+        /^\/(\d+)\/?$/
+    ];
+
     try {
         url = new URL(url);
         const path = url.pathname;
         let fbid = null;
 
-        if (path === "/permalink.php") {
-            fbid = url.searchParams.get("story_fbid");
-        } else {
-            for (const regex of [
-                /^\/(?:.+)\/posts\/(\d+)\/?$/,
-                /^\/groups\/(?:.+)\/user\/(\d+)\/?$/,
-                /^\/(\d+)\/?$/
-            ]) {
+        for(const [m, r] of rules) {
+            if (m(url)) {
+                fbid = r(url);
+                break;
+            }
+        }
+
+        if (fbid === null) {
+            for (const regex of regexs) {
                 if (regex.test(path)) {
                     fbid = path.match(regex)[1];
                     break
